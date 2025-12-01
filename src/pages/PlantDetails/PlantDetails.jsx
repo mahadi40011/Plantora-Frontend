9d901550ae9f3bb49ae74eb4e1fcd9fd8a6b8aaa
mahadi40011 <mahadi40011@gmail.com>
2025-12-01 16:25:44 +0600

    Created a functoion for payment, and post paymentInfo to server using Axios

--- src/pages/PlantDetails/PlantDetails.jsx
+++ src/pages/PlantDetails/PlantDetails.jsx
@@ -100,7 +100,7 @@ const PlantDetails = () => {
           </div>
           <hr className="my-6" />
 
-          <PurchaseModal closeModal={closeModal} isOpen={isOpen} />
+          <PurchaseModal plant={plant} closeModal={closeModal} isOpen={isOpen} />
         </div>
       </div>
     </Container>
